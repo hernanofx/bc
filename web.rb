22147b26c6bb95b5eec84ddb820468871142b7fd
hernanofx <hernanofx@hotmail.com>
2016-08-24 13:37:56 -0300

    paginas interiores y menu

--- web.rb
+++ web.rb
@@ -12,3 +12,39 @@ end
 get '/home' do
   erb :index
 end
+
+get '/musica' do
+  erb :musica
+end
+
+get '/audios' do
+  erb :audios
+end
+
+get '/videos' do
+  erb :videos
+end
+
+get '/letras' do
+  erb :letras
+end
+
+get '/recitales' do
+  erb :recitales
+end
+
+get '/proximos' do
+  erb :proximos
+end
+
+get '/pasados' do
+  erb :pasados
+end
+
+get '/entradas' do
+  erb :entradas
+end
+
+get '/merchandising' do
+  erb :merchandising
+end
