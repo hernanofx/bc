3e270e48288a2bd4456bf68fef8cde894caf8014
hernanofx <hernanofx@hotmail.com>
2016-08-25 14:54:30 -0300

    estilos en entradas

--- public/js/entradas.js
+++ public/js/entradas.js
@@ -1,6 +1,6 @@
 // When the user clicks anywhere outside of the modal, close it
 var modal = document.getElementById('ticketModal');
-window.onclick = function (event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
